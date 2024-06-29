b8a91e1390e27e005495b08077bbeab56a42751b
Pavel <37714428+pavel1551@users.noreply.github.com>
2024-06-29 14:06:18 +0400

    задание по теме "Переменные"
    задание по теме "Переменные"

---
+++ homework2.py
@@ -0,0 +1,8 @@
+The_numberof_completed_DZ = 12 #Количество выполненных ДЗ
+Number_of_hours_spent = 1.5 #Количество затраченных часов
+Course_name = "Python" #Название курса
+Time_for_one_task = Number_of_hours_spent/The_numberof_completed_DZ #Время на одно задание
+print('Курс:', Course_name, ', всего задач:', The_numberof_completed_DZ, ', затрачено часов:', Number_of_hours_spent, ', среднее время выполнения', Time_for_one_task, 'часа.')
+
+# Можно использовать F строки
+#print(f'Курс: {Course_name} всего задач: {The_numberof_completed_DZ}, затрачено часов: {Number_of_hours_spent}, среднее время выполнения {Time_for_one_task} часа.')
