b2545e73daeaaddc086eaa00e70f99bec77d567b
Pavel <37714428+pavel1551@users.noreply.github.com>
2024-07-23 18:32:00 +0400

    Инфо. Стиль кода часть II. Цикл While.
    Информация. Стиль кода часть II. Цикл While.

---
+++ info_module_2_3.py
@@ -0,0 +1,19 @@
+# number = int(input("Ведите число: "))
+# if number %2 == 0:
+#     print("Число чётное")
+# else:
+#     print("Число не чётное")
+
+# Цикл While
+
+while 1 > 0:
+    number = int(input("Ведите число: "))
+    if number % 2 == 0:
+        print("Число чётное")
+        # continue # пропускает выполнение всех остальных команд
+    else:
+        print("Число не чётное")
+        break #Выход из цикла
+print("Я за циклом")
+
+
